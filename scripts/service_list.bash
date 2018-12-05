f6ed9f8cbb043ff58cd33a82ac064132acffbf9b
Faruk D <f.diblen@esciencecenter.nl>
2018-12-05 12:05:22 +0100

    show network information, container state and list stacks

--- scripts/service_list.bash
+++ scripts/service_list.bash
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-stacks=( 'spark' 'minio' 'vis' )
+echo '------------------------------------------------------------------------------------'
+echo 'CONTAINERS'
+docker ps --format "table {{.Names}}\t {{.Ports}}"
 
+
+echo '------------------------------------------------------------------------------------'
+echo 'IP Addresses'
+# for service in $(docker service ls -q);
+# do
+#     docker service ps $service -q --filter "desired-state=Running" | xargs docker inspect --format '{{range .NetworksAttachments}}{{.Addresses}}{{end}}';
+# done
+docker inspect -f 'table {{.Name}}  {{range .NetworkSettings.Networks}} {{.IPAddress}}{{end}}' $(docker ps -aq)
+
+
+echo '------------------------------------------------------------------------------------'
+echo 'STACKS'
+stacks=( 'spark' 'minio' 'vis' )
 for stackName in "${stacks[@]}"
 do
-    echo '------------------------------------------------------------------------------------'
-    docker stack ps --format 'table {{.Name}}\t{{.Node}}\t{{.CurrentState}}' $stackName
+    echo ''
+    docker stack ps --format 'table {{.ID}}\t{{.Name}}\t{{.Node}}\t{{.CurrentState}}' $stackName
 done
